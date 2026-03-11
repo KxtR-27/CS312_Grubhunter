bd6eec95b1f37d0082222925baf6858df8683c5b
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-03-11 08:43:09 -0400

    Indicate incomplete code sections

--- code/custom.d.ts
+++ code/custom.d.ts
@@ -1,5 +1,9 @@
 import mongoose, { Mongoose } from "mongoose";
 
+// NOTE: this part is incomplete.
+// We have yet to discuss this portion of the activity.
+// We have only discussed through `grubHunterActivity1.pdf`,
+// but this part pertains to `grubHunterActivity**2**.pdf`.
 declare global {
     var mongoose
 }
