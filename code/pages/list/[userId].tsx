8140ae6393eef9ce2f335991796f0c330a9adfb4
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-04-17 22:56:54 -0400

    (Activity 7) add wishlist page (that knows if it's yours!)

---
+++ code/pages/list/[userId].tsx
@@ -0,0 +1,57 @@
+import LocationsList from "@/grubhunter-application/components/locations-list";
+import dbConnect from "@/middleware/mongodb-connection";
+import { onWishlist } from "@/mongoose/locations/services";
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import type { Location } from "@/mongoose/locations/schema";
+
+interface WishlistDetailsContext extends GetServerSidePropsContext {
+	params: { userId: string };
+}
+
+const WishlistPage: NextPage = ({
+	serializedLocations,
+	userId,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+	const locations = serializedLocations as Location[];
+
+	const { data: session, status } = useSession();
+
+	const title = `${userId}'s Wishlist`;
+    const wishlistBelongsToLoggedInUser = status === "authenticated" && userId === session.user.name
+    const wishlistIsEmpty = !locations || locations.length === 0;
+
+    return (
+		<>
+			<Head>
+				<title>{title}</title>
+			</Head>
+			<main>
+				<h1>{wishlistBelongsToLoggedInUser ? "Your" : `${userId}'s`} Wishlist</h1>
+                {
+                    wishlistIsEmpty
+                    ? <p>Oops! There's nothing here!</p>
+                    : <LocationsList locations={locations} />
+                }
+			</main>
+		</>
+	);
+};
+
+const getServerSideProps: GetServerSideProps = async context => {
+	const userId = (context as WishlistDetailsContext).params.userId;
+
+	await dbConnect();
+	const locations = await onWishlist(userId).catch(error => {
+        console.error(error);
+        return [];
+    });
+	// ↓ widely regarded as best practice. serializes an object. ↓
+	const serializedLocations = JSON.parse(JSON.stringify(locations));
+
+	return { props: { serializedLocations, userId } };
+};
+
+export default WishlistPage;
+export { getServerSideProps };
