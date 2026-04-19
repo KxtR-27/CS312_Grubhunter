c9e6b2e42fdb765f98a3c4e1c6841104df5684d8
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-04-18 21:32:30 -0400

    Fix mongoose bugs
    1. Mongoose was attempting to overwrite the model every time it was needed.
    2. the typing of the `on_wishlist` key in the locationSchema was incorrectly typed as `Array<String>` instead of `[String]`. this caused typescript to view it as an Array<String>.prototype

--- code/mongoose/locations/schema.ts
+++ code/mongoose/locations/schema.ts
@@ -7,7 +7,7 @@ const locationSchema = new Schema({
 	cuisine: { type: String, required: true },
 	grade: { type: String, required: true },
 	name: { type: String, required: true },
-	on_wishlist: { type: Array<String>, required: true },
+	on_wishlist: { type: [String], required: true },
 	location_id: { type: String, required: true },
 });
 
